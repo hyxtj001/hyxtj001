499112f25e198fc7f35e2892dd938e4cad7b3f45
Yuxin Huang <yuxinhuang@yuxindemacbook-air.local>
2017-02-02 19:34:08 +0800

    Initial Commit

---
+++ A1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  A1
+//
+//  Created by Yuxin Huang on 17/2/2.
+//  Copyright © 2017年 Yuxin Huang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ A1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  A1
+//
+//  Created by Yuxin Huang on 17/2/2.
+//  Copyright © 2017年 Yuxin Huang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
